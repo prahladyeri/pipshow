da9475b7927de8517fb275b1a0759b2e4f67606f
Prahlad Yeri <prahladyeri@yahoo.com>
2019-05-10 20:58:28 +0530

    updated readme, fixed typos

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
 	name="pipshow",
-	version="0.2",
+	version="0.5",
 	license="MIT",
 	description="A script to show details of any python package, irrespective of whether its installed or not.",
+	long_description=read("README.md"),
+	long_description_content_type='text/markdown',
 	keywords="pip,setup,installer",
 	url="https://github.com/prahladyeri/pipshow",
 	packages=find_packages(),
