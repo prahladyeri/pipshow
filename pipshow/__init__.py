1decfd4005f957fdc9f101c20a80f538dd8064f4
Prahlad Yeri <prahladyeri@yahoo.com>
2019-06-14 21:03:19 +0530

    chore: added missing attributes to __init__.py

--- pipshow/__init__.py
+++ pipshow/__init__.py
@@ -1,2 +1,8 @@
+__author__ = "Prahlad Yeri"
+__email__ = "prahladyeri@yahoo.com"
+__copyright__ = "(c) 2019 Prahlad Yeri"
+
 __version__ = "1.0.5"
+__license__ = "MIT"
+__title__ = 'pipshow'
 __description__ = "A script to show details of any python package, irrespective of whether its installed or not"
