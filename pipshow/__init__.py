5e82d92668cbdcfde90b29ecb9a202cf11977112
Prahlad Yeri <prahladyeri@yahoo.com>
2019-12-11 10:14:15 +0530

    fix: added README to installation package, added MANIFEST.in

--- pipshow/__init__.py
+++ pipshow/__init__.py
@@ -2,7 +2,7 @@ __author__ = "Prahlad Yeri"
 __email__ = "prahladyeri@yahoo.com"
 __copyright__ = "(c) 2019 Prahlad Yeri"
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 __license__ = "MIT"
 __title__ = 'pipshow'
 __description__ = "A script to show details of any python package, irrespective of whether its installed or not"
